8a145637ce1a037ebeaad0c2eaccd9c6088f93ef
MarineMeyZielinski <m.zielinski.webdev@gmail.com>
2025-09-04 13:24:28 +0200

    Add SDK Firebase

---
+++ script.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCQTM0ru-Z10RKx9IJXYisTRE2XWQ1RBfI",
+  authDomain: "blog-musique-mey-chrystelle.firebaseapp.com",
+  projectId: "blog-musique-mey-chrystelle",
+  storageBucket: "blog-musique-mey-chrystelle.firebasestorage.app",
+  messagingSenderId: "867287070944",
+  appId: "1:867287070944:web:21cc2034f49c97fb1c3885",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
